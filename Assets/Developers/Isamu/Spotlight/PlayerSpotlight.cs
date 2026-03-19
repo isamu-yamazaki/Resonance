9c066c863ec7bdf57b878c4c7606d31fc3a4a948
Isamu <isamu.dy2@gmail.com>
2026-03-18 21:16:19 -0700

    configured spotlight

--- Assets/Developers/Isamu/Spotlight/PlayerSpotlight.cs
+++ Assets/Developers/Isamu/Spotlight/PlayerSpotlight.cs
@@ -1,75 +1,111 @@
 using PurrNet;
 using UnityEngine;
-using UnityEngine.VFX;
 
 namespace Resonance
 {
     public class PlayerSpotlight : NetworkBehaviour
     {
-        [Header("VFX")]
-        [SerializeField] private VisualEffect spotlightEffectPrefab;
-        [SerializeField] private Vector3 localOffset = new Vector3(0f, 2.5f, 0f);
+        [Header("References")]
+        [SerializeField] private Material spotlightMaterial;
 
         [Header("Appearance")]
-        [SerializeField] private Color spotlightColor = new Color(0.6f, 0.85f, 1f, 1f);
-        [SerializeField] private float spotlightAlpha = 0.35f;
+        [SerializeField] private Color beamColor = new Color(0.9f, 0.95f, 1f, 1f);
 
-        private VisualEffect spotlightInstance;
+        [Header("Beam (Visual)")]
+        [SerializeField] private float beamIntensity = 600f;
+        [SerializeField] private float beamWidth = 3f;
+        [SerializeField] private float beamHeight = 20f;
 
-        // VFX Graph exposed property names
-        private static readonly int colorPropertyID = Shader.PropertyToID("SpotlightColor");
-        private static readonly int alphaPropertyID = Shader.PropertyToID("SpotlightAlpha");
+        [Header("Light (Ground)")]
+        [SerializeField] private float lightIntensity = 5f;
+        [SerializeField] private float lightRange = 25f;
+        [SerializeField] private float lightSpotAngle = 30f;
+
+        private GameObject beamObject;
+        private GameObject lightObject;
+        private Material beamMaterialInstance;
 
         protected override void OnSpawned(bool asServer)
         {
             base.OnSpawned(asServer);
 
             if (!asServer)
-                SpawnSpotlight();
+            {
+                SpawnBeam();
+                SpawnLight();
+            }
         }
 
         protected override void OnDespawned(bool asServer)
         {
             base.OnDespawned(asServer);
 
-            if (spotlightInstance != null)
-                Destroy(spotlightInstance.gameObject);
+            if (beamMaterialInstance != null)
+                Destroy(beamMaterialInstance);
+            if (beamObject != null)
+                Destroy(beamObject);
+            if (lightObject != null)
+                Destroy(lightObject);
         }
 
-        private void SpawnSpotlight()
+        private void SpawnBeam()
         {
-            if (spotlightEffectPrefab == null)
+            if (spotlightMaterial == null)
             {
-                Debug.LogError("[PlayerSpotlight] No VFX effect prefab assigned!");
+                Debug.LogError("[PlayerSpotlight] No spotlight material assigned!");
                 return;
             }
 
-            spotlightInstance = Instantiate(spotlightEffectPrefab, transform);
-            spotlightInstance.transform.localPosition = localOffset;
-            spotlightInstance.transform.localRotation = Quaternion.identity;
-            spotlightInstance.gameObject.name = "PlayerSpotlight_VFX";
+            beamObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            beamObject.name = "PlayerSpotlight_Beam";
+            beamObject.transform.SetParent(transform);
+            beamObject.transform.localPosition = new Vector3(0f, beamHeight * 0.5f, 0f);
+            beamObject.transform.localRotation = Quaternion.identity;
+            beamObject.transform.localScale = new Vector3(beamWidth, beamHeight * 0.5f, beamWidth);
 
-            ApplyVisualProperties();
-            spotlightInstance.Play();
+            Destroy(beamObject.GetComponent<CapsuleCollider>());
+
+            beamMaterialInstance = new Material(spotlightMaterial);
+            beamMaterialInstance.SetColor("_Color", beamColor);
+            beamMaterialInstance.SetFloat("_Intensity", beamIntensity);
+            beamObject.GetComponent<MeshRenderer>().material = beamMaterialInstance;
         }
 
-        private void ApplyVisualProperties()
+        private void SpawnLight()
         {
-            if (spotlightInstance == null) return;
+            lightObject = new GameObject("PlayerSpotlight_Light");
+            lightObject.transform.SetParent(transform);
+            lightObject.transform.localPosition = new Vector3(0f, beamHeight, 0f);
+            lightObject.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
 
-            if (spotlightInstance.HasVector4(colorPropertyID))
-                spotlightInstance.SetVector4(colorPropertyID, spotlightColor);
-
-            if (spotlightInstance.HasFloat(alphaPropertyID))
-                spotlightInstance.SetFloat(alphaPropertyID, spotlightAlpha);
+            Light spotlight = lightObject.AddComponent<Light>();
+            spotlight.type = LightType.Spot;
+            spotlight.color = beamColor;
+            spotlight.intensity = lightIntensity;
+            spotlight.range = lightRange;
+            spotlight.spotAngle = lightSpotAngle;
         }
 
 #if UNITY_EDITOR
         private void OnValidate()
         {
-            // Live-update in editor when tweaking color/alpha
-            if (spotlightInstance != null)
-                ApplyVisualProperties();
+            if (beamMaterialInstance != null)
+            {
+                beamMaterialInstance.SetColor("_Color", beamColor);
+                beamMaterialInstance.SetFloat("_Intensity", beamIntensity);
+            }
+
+            if (lightObject != null)
+            {
+                Light spotlight = lightObject.GetComponent<Light>();
+                if (spotlight != null)
+                {
+                    spotlight.color = beamColor;
+                    spotlight.intensity = lightIntensity;
+                    spotlight.range = lightRange;
+                    spotlight.spotAngle = lightSpotAngle;
+                }
+            }
         }
 #endif
     }
