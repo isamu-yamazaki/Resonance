788ccc1dbcd7ea3b6eb608a159fff6e8fd11f1b7
Brendan Chen <me@bchen.dev>
2026-03-23 10:50:25 -0700

    Automatically register lobby member ID with player ID, and fix server auth

--- Assets/Scripts/Lobby/PlayerIdToLobbyMemberIdMap.cs
+++ Assets/Scripts/Lobby/PlayerIdToLobbyMemberIdMap.cs
@@ -9,9 +9,10 @@ namespace Resonance.LobbySystem
 
         private LobbyDataHolder lobbyDataHolder;
 
-        public void RegisterLobbyMemberIdWithPlayerId(PlayerID playerId)
+        [ServerRpc(requireOwnership: false)]
+        private void RegisterLobbyMemberIdWithPlayerId(PlayerID playerId, string lobbyMemberId)
         {
-            lobbyMemberIdsByPlayerId.Add(playerId, lobbyDataHolder.LocalUserId);
+            lobbyMemberIdsByPlayerId.Add(playerId, lobbyMemberId);
         }
 
         public string GetLobbyMemberId(PlayerID playerId)
@@ -40,12 +41,29 @@ namespace Resonance.LobbySystem
             DontDestroyOnLoad(this);
         }
 
+        protected override void OnSpawned(bool asServer)
+        {
+            base.OnSpawned(asServer);
+
+            if (!asServer)
+            {
+                RegisterLobbyMemberIdWithPlayerId(networkManager.localPlayer, lobbyDataHolder.LocalUserId);
+            }
+        }
+
         protected override void OnDestroy()
         {
             if (InstanceHandler.TryGetInstance<PlayerIdToLobbyMemberIdMap>(out var _))
             {
                 InstanceHandler.UnregisterInstance<PlayerIdToLobbyMemberIdMap>();
             }
+        }
+
+        [ContextMenu("Get lobby member ID of local player ID")]
+        private void TestGetLobbyMemberIdOfLocalPlayerId()
+        {
+            var lobbyMemberId = GetLobbyMemberId(networkManager.localPlayer);
+            Debug.Log($"[PlayerIdToLobbyMemberIdMap] Lobby member ID of local player ID: {lobbyMemberId}");
         }
     }
 }
