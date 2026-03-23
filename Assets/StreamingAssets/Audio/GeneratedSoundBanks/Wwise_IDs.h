898314c8925ed358dea86d9fa169bdeea09d81bc
Isamu Dorr-Yamazaki <isamu.dy2@gmail.com>
2026-03-23 15:02:37 -0700

    reimplement grass surface sfx to fix merge conflicts

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -37,6 +37,7 @@ namespace AK
             namespace SWITCH
             {
                 static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
                 static const AkUniqueID GRAVEL = 2185786256U;
                 static const AkUniqueID METAL = 2473969246U;
                 static const AkUniqueID WOOD = 2058049674U;
