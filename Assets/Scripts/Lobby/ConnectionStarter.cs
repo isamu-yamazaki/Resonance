83fc7df9250ed41cba14dbed2e982fc82fda13d2
Brendan Chen <me@bchen.dev>
2026-03-25 11:07:46 -0700

    Change ConnectionStarter to read the build config and start NetworkManager conditionally

--- Assets/Scripts/Lobby/ConnectionStarter.cs
+++ Assets/Scripts/Lobby/ConnectionStarter.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using PurrNet;
 using PurrNet.Logging;
 using PurrNet.Transports;
+using PurrNet.Utils;
+using Resonance.BuildTools;
 using UnityEngine;
 
 #if UTP_LOBBYRELAY
@@ -59,13 +61,51 @@ namespace Resonance.LobbySystem
                 (_networkManager.transport as UTPTransport).InitializeRelayClient(_lobbyDataHolder.CurrentLobby.Properties["JoinCode"]);
             }
 #else
-                //P2P Connection, receive IP/Port from server
+                // P2P Connection, receive IP/Port from server
 #endif
 
-            if(_lobbyDataHolder.CurrentLobby.IsOwner(_lobbyDataHolder.LocalUserId))
+            if (ShouldStartAsServer)
+            {
                 _networkManager.StartServer();
-            StartCoroutine(StartClient());
+            }
+            else if (ShouldStartAsHost)
+            {
+                _networkManager.StartHost();
+            }
+            else if (ShouldStartAsClient)
+            {
+                StartCoroutine(StartClient());
+            }
+            else
+            {
+                PurrLogger.LogError("Could not determine a network start mode. Check that a build config receiver is present in the scene.", this);
+            }
         }
+
+        #region Build Context
+
+        private bool HasClientConfig => ClientBuildConfigReceiver.Instance != null;
+        private bool HasServerConfig => ServerBuildConfigReceiver.Instance != null;
+        private bool IsClientServerMode => HasClientConfig && ClientBuildConfigReceiver.Instance.Config.useClientServerMode;
+        private bool IsHostMode => HasClientConfig && !ClientBuildConfigReceiver.Instance.Config.useClientServerMode;
+        private bool IsLobbyOwner => _lobbyDataHolder.CurrentLobby.IsOwner(_lobbyDataHolder.LocalUserId);
+
+        #endregion
+
+        #region Outcome Predicates
+
+        private bool ShouldStartAsServer =>
+            HasServerConfig ||
+            (IsClientServerMode && ApplicationContext.isMainEditor);
+
+        private bool ShouldStartAsHost =>
+            IsHostMode && IsLobbyOwner;
+
+        private bool ShouldStartAsClient =>
+            (IsClientServerMode && !ApplicationContext.isMainEditor) ||
+            (IsHostMode && !IsLobbyOwner);
+
+        #endregion
 
         private IEnumerator StartClient()
         {
