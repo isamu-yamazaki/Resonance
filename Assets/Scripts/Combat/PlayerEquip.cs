44c534eba3d9688009bb67c577e48aac69207925
Isamu <isamu.dy2@gmail.com>
2026-03-31 00:07:27 -0700

    Update PlayerEquip.cs

--- Assets/Scripts/Combat/PlayerEquip.cs
+++ Assets/Scripts/Combat/PlayerEquip.cs
@@ -45,15 +45,15 @@ namespace Resonance.Combat
         {
             base.OnSpawned();
             enabled = isOwner;
+        }
 
-            if (isOwner)
-            {
-                playerSkinRenderer = GetComponent<PlayerSkinRenderer>();
-                playerSkinRenderer.OnNewSkinSpawned += UpdateEquipSlotFromSkin;
+        private void Awake()
+        {
+            playerSkinRenderer = GetComponent<PlayerSkinRenderer>();
+            playerSkinRenderer.OnNewSkinSpawned += UpdateEquipSlotFromSkin;
 
-                weapons = Resources.LoadAll<WeaponProperties>("Content/Weapons");
-                playerState = GetComponent<PlayerState>();
-            }
+            weapons = Resources.LoadAll<WeaponProperties>("Content/Weapons");
+            playerState = GetComponent<PlayerState>();
         }
 
         private void Start()
@@ -310,4 +310,3 @@ namespace Resonance.Combat
         }
     }
 }
-
