4932b9ee8c7d9875f0c0bd35c885ea3c4274eb8f
Brendan Chen <me@bchen.dev>
2026-04-01 12:05:03 -0700

    Also put MatchMovementLock and PlayerAnimation behind owner gates

--- Assets/Scripts/PlayerController/MatchMovementLock.cs
+++ Assets/Scripts/PlayerController/MatchMovementLock.cs
@@ -1,13 +1,21 @@
+using PurrNet;
 using Resonance.Assemblies.SharedGameLogic;
 using Resonance.Match;
 using UnityEngine;
 
 namespace Resonance.PlayerController
 {
-    public class MatchMovementLock : MonoBehaviour
+    public class MatchMovementLock : NetworkBehaviour
     {
         private BaseRoundManagerNetworkAdapter roundManager;
         private PlayerState playerState;
+
+        protected override void OnSpawned()
+        {
+            base.OnSpawned();
+            enabled = isOwner;
+        }
+
         private void Awake()
         {
             roundManager = MatchLogicNetworkAdapter.Instance?.ActiveRoundManager;
