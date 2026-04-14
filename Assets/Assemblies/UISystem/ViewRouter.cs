f79692bbeab624694913b68d11499c29bd9c5387
Brendan Chen <me@bchen.dev>
2026-04-13 17:58:49 -0700

    Add RegisterOverlay and HideOverlay

--- Assets/Assemblies/UISystem/ViewRouter.cs
+++ Assets/Assemblies/UISystem/ViewRouter.cs
@@ -11,6 +11,21 @@ namespace Resonance.Assemblies.UISystem
         // note that because this just toggles on/off existing game objects,
         // z-ordering is done in the Unity editor
 
+        private int nextOverlayId = 0;
+
+        private int GetNewOverlayId()
+        {
+            nextOverlayId++;
+            return nextOverlayId;
+        }
+
+        public int RegisterOverlay(OverlayOptions options)
+        {
+            var id = GetNewOverlayId();
+            overlays.Add(id, options);
+            return id;
+        }
+
         public void ShowOverlay(int id)
         {
             if (activeOverlayIds.Contains(id)) return;
@@ -21,6 +36,18 @@ namespace Resonance.Assemblies.UISystem
 
             activeOverlayIds.Add(id);
             overlays[id].view.Show();
+        }
+
+        public void HideOverlay(int id)
+        {
+            if (!activeOverlayIds.Contains(id)) return;
+            if (!overlays.ContainsKey(id))
+            {
+                throw new KeyNotFoundException("Overlay ID not registered");
+            }
+
+            activeOverlayIds.Remove(id);
+            overlays[id].view.Hide();
         }
     }
 }
