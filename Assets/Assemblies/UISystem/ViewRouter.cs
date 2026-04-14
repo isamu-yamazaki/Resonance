50674046459e5f68b166207ba49326722311e1e2
Brendan Chen <me@bchen.dev>
2026-04-13 17:53:25 -0700

    Implement base properties and ShowOverlay method

--- Assets/Assemblies/UISystem/ViewRouter.cs
+++ Assets/Assemblies/UISystem/ViewRouter.cs
@@ -1,19 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Resonance.Assemblies.UISystem
 {
     public abstract class ViewRouter : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private Dictionary<int, OverlayOptions> overlays = new();
+        private HashSet<int> activeOverlayIds = new();
+
+        // note that because this just toggles on/off existing game objects,
+        // z-ordering is done in the Unity editor
+
+        public void ShowOverlay(int id)
         {
+            if (activeOverlayIds.Contains(id)) return;
+            if (!overlays.ContainsKey(id))
+            {
+                throw new KeyNotFoundException("Overlay ID not registered");
+            }
 
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            activeOverlayIds.Add(id);
+            overlays[id].view.Show();
         }
     }
 }
