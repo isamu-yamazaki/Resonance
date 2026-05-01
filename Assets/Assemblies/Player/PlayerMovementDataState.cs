99e6422bb2ecd6e3e0b09a6760af9f42b03a6baf
Brendan Chen <me@bchen.dev>
2026-04-30 17:07:30 -0700

    Add CameraYaw to PlayerMovementDataState

--- Assets/Assemblies/Player/PlayerMovementDataState.cs
+++ Assets/Assemblies/Player/PlayerMovementDataState.cs
@@ -4,6 +4,12 @@ namespace Resonance.Assemblies.Player
 {
     public struct PlayerMovementDataState
     {
-        public Vector3 velocity;
+        public Vector3 Velocity;
+
+        /// <summary>
+        /// The x-component of the camera rotation.
+        /// Modified deterministically each tick by PlayerInputData.LookInput.
+        /// </summary>
+        public float CameraYaw;
     }
 }
