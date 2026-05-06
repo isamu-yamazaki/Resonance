eb0c92fd5efe9498d18203ba3d49812ba003fe29
Brendan Chen <me@bchen.dev>
2026-05-06 12:53:26 -0700

    Change references in PreMatchCameraController

--- Assets/Scripts/Camera/PreMatchCameraController.cs
+++ Assets/Scripts/Camera/PreMatchCameraController.cs
@@ -101,14 +101,14 @@ public class PreMatchCameraController : MonoBehaviour
 
     private IEnumerator Start()
     {
-        while (PlayerController.LocalPlayer == null || !_configured)
+        while (PlayerPredictedController.LocalPlayer == null || !_configured)
             yield return null;
 
         if (hudCanvas == null)
             hudCanvas = GameObject.FindWithTag("HUDCanvas");
         
-        target = PlayerController.LocalPlayer.transform;
-        playerCamera = PlayerController.LocalPlayer.GetComponentInChildren<Camera>(true);
+        target = PlayerPredictedController.LocalPlayer.transform;
+        playerCamera = PlayerPredictedController.LocalPlayer.GetComponentInChildren<Camera>(true);
 
         Task<int> matchStateTask = ArenaRoundManagerBridge.GetTemporaryReference()?.GetMatchState();
         if (matchStateTask == null)
